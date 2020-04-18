7fbb322fda58ee1401bb89911551d2253d59642d
Steven Tuet Piu Kwok <mrfishball@gmail.com>
2020-04-18 15:36:36 -0400

    Update link.sh

--- link.sh
+++ link.sh
@@ -1,57 +1,154 @@
 #!/usr/bin/env bash
 
-DOTFILES=$HOME/dotfiles
+answer_is_yes() {
+    [[ "$REPLY" =~ ^[Yy]$ ]] \
+        && return 0 \
+        || return 1
+}
 
-echo -e "\\nCreating symlinks"
-echo "=============================="
-linkables=$( find -H "$DOTFILES" -maxdepth 3 -name '*.symlink' )
-for file in $linkables ; do
-    target="$HOME/.$( basename "$file" '.symlink' )"
-    if [ -e "$target" ]; then
-        echo "~${target#$HOME} already exists... Skipping."
-    else
-        echo "Creating symlink for $file"
-        ln -s "$file" "$target"
+ask() {
+    print_question "$1"
+    read
+}
+
+ask_for_confirmation() {
+    print_question "$1 (y/n) "
+    read -n 1
+    printf "\n"
+}
+
+ask_for_sudo() {
+
+    # Ask for the administrator password upfront
+    sudo -v
+
+    # Update existing `sudo` time stamp until this script has finished
+    # https://gist.github.com/cowboy/3118588
+    while true; do
+        sudo -n true
+        sleep 60
+        kill -0 "$$" || exit
+    done &> /dev/null &
+
+}
+
+cmd_exists() {
+    [ -x "$(command -v "$1")" ] \
+        && printf 0 \
+        || printf 1
+}
+
+execute() {
+    $1 &> /dev/null
+    print_result $? "${2:-$1}"
+}
+
+get_answer() {
+    printf "$REPLY"
+}
+
+get_os() {
+
+    declare -r OS_NAME="$(uname -s)"
+    local os=""
+
+    if [ "$OS_NAME" == "Darwin" ]; then
+        os="osx"
+    elif [ "$OS_NAME" == "Linux" ] && [ -e "/etc/lsb-release" ]; then
+        os="ubuntu"
     fi
-done
 
-echo -e "\\n\\ninstalling to ~/.config"
-echo "=============================="
-if [ ! -d "$HOME/.config" ]; then
-    echo "Creating ~/.config"
-    mkdir -p "$HOME/.config"
-fi
+    printf "%s" "$os"
 
-config_files=$( find "$DOTFILES/config" -maxdepth 1 2>/dev/null )
-for config in $config_files; do
-    target="$HOME/.config/$( basename "$config" )"
-    if [ -e "$target" ]; then
-        echo "~${target#$HOME} already exists... Skipping."
-    else
-        echo "Creating symlink for $config"
-        ln -s "$config" "$target"
+}
+
+is_git_repository() {
+    [ "$(git rev-parse &>/dev/null; printf $?)" -eq 0 ] \
+        && return 0 \
+        || return 1
+}
+
+mkd() {
+    if [ -n "$1" ]; then
+        if [ -e "$1" ]; then
+            if [ ! -d "$1" ]; then
+                print_error "$1 - a file with the same name already exists!"
+            else
+                print_success "$1"
+            fi
+        else
+            execute "mkdir -p $1" "$1"
+        fi
     fi
-done
+}
 
-# create vim symlinks
-# As I have moved off of vim as my full time editor in favor of neovim,
-# I feel it doesn't make sense to leave my vimrc intact in the dotfiles repo
-# as it is not really being actively maintained. However, I would still
-# like to configure vim, so lets symlink ~/.vimrc and ~/.vim over to their
-# neovim equivalent.
+print_error() {
+    # Print output in red
+    printf "\e[0;31m  [✖] $1 $2\e[0m\n"
+}
 
-# echo -e "\\n\\nCreating vim symlinks"
-# echo "=============================="
-# VIMFILES=( "$HOME/.vim:$DOTFILES/config/nvim"
-#         "$HOME/.vimrc:$DOTFILES/config/nvim/init.vim" )
-#
-# for file in "${VIMFILES[@]}"; do
-#     KEY=${file%%:*}
-#     VALUE=${file#*:}
-#     if [ -e "${KEY}" ]; then
-#         echo "${KEY} already exists... skipping."
-#     else
-#         echo "Creating symlink for $KEY"
-#         ln -s "${VALUE}" "${KEY}"
-#     fi
-# done
+print_info() {
+    # Print output in purple
+    printf "\n\e[0;35m $1\e[0m\n\n"
+}
+
+print_question() {
+    # Print output in yellow
+    printf "\e[0;33m  [?] $1\e[0m"
+}
+
+print_result() {
+    [ $1 -eq 0 ] \
+        && print_success "$2" \
+        || print_error "$2"
+
+    [ "$3" == "true" ] && [ $1 -ne 0 ] \
+        && exit
+}
+
+print_success() {
+    # Print output in green
+    printf "\e[0;32m  [✔] $1\e[0m\n"
+}
+
+# finds all .dotfiles in this folder
+declare -a FILES_TO_SYMLINK=$(find . -type f -maxdepth 1 -name ".*" -not -name .DS_Store -not -name .git -not -name .osx | sed -e 's|//|/|' | sed -e 's|./.|.|')
+# FILES_TO_SYMLINK="$FILES_TO_SYMLINK .vim bin .config/fish" # add in vim and the binaries
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+main() {
+
+    local i=""
+    local sourceFile=""
+    local targetFile=""
+
+    for i in ${FILES_TO_SYMLINK[@]}; do
+
+        sourceFile="$(pwd)/$i"
+        targetFile="$HOME/$(printf "%s" "$i" | sed "s/.*\/\(.*\)/\1/g")"
+
+        if [ -e "$targetFile" ]; then
+            if [ "$(readlink "$targetFile")" != "$sourceFile" ]; then
+
+                ask_for_confirmation "'$targetFile' already exists, do you want to overwrite it?"
+                if answer_is_yes; then
+                    rm -rf "$targetFile"
+                    execute "ln -fs $sourceFile $targetFile" "$targetFile → $sourceFile"
+                else
+                    print_error "$targetFile → $sourceFile"
+                fi
+
+            else
+                print_success "$targetFile → $sourceFile"
+            fi
+        else
+            execute "ln -fs $sourceFile $targetFile" "$targetFile → $sourceFile"
+        fi
+
+    done
+
+}
+
+main
